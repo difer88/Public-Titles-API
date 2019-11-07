5843e41cc738a73f13c413e3259628c5a4d939f6
Diego Fernandes <diegofernandes88@hotmail.com>
2019-11-06 21:58:48 -0300

    Initial project creation / Maven configuration

---
+++ src/main/java/com/diegofernandes/webdatacollector/WebdatacollectorApplication.java
@@ -0,0 +1,13 @@
+package com.diegofernandes.webdatacollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdatacollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdatacollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diegofernandes/webdatacollector/WebdatacollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegofernandes.webdatacollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebdatacollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
