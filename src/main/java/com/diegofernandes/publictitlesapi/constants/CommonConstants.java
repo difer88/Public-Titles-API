f8a3065a4fd725341d57e21f254d9e35603f5fad
Diego Fernandes <diegofernandes88@hotmail.com>
2019-11-17 18:45:52 -0300

    Implementation of Data File Ingestion by URL

--- src/main/java/com/diegofernandes/publictitlesapi/constants/CommonConstants.java
+++ src/main/java/com/diegofernandes/publictitlesapi/constants/CommonConstants.java
@@ -4,5 +4,6 @@ public final class CommonConstants {
 
     public static final String URL_TESOURO_DIRETO = "http://www.tesouro.fazenda.gov.br/tesouro-direto-precos-e-taxas-dos-titulos";
     public static final String USER_AGENT = "Chrome";
+    public static final String STATUS_DATA_INGESTION_SUCCESS = "Arquivo ingerido com sucesso";
 
 }
